018ea68cf1199ceb5a92bde0ede4e6cb7c2d4ba4
Pratham Bhadana <prathambhadana3@gmail.com>
2026-06-28 17:44:01 +0530

    Update programs - 2026-06-28

--- TwoDarrays.java
+++ TwoDarrays.java
@@ -16,29 +16,85 @@ public class TwoDarrays {
         System.out.print(" key not found ");
         return false;
     }
-    public static void main(String args[]){
-        
-        int matrix [][]= new int[3][3];
+
+
+
+
+        public static int count(int[][] matrix){
         int n = matrix.length;
         int m = matrix[0].length;
+        int count = 0;
 
-        Scanner sc = new Scanner(System.in);
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                matrix[i][j]= sc.nextInt();
+                if(matrix[i][j] == 7){
+                    count+=1;
+                }
+            }
+        }System.out.println(count);
+        return count;
+        
+    }
+
+    public static int[][] transpose(int[][] matrix) {
+    int n = matrix.length;
+    int m = matrix[0].length;
+
+    int[][] transposed = new int[m][n];  // dimensions flipped
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            transposed[j][i] = matrix[i][j];  // assignment direction correct
+        }
+    }
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            System.out.print("[" + transposed[i][j] + "]");  // assignment direction correct
+        }System.out.println();
+    }
+    return transposed;
+}
+
+
+
+    public static int Sum(int[][] matrix){
+        int sum = 0, n = matrix.length, m = matrix[1].length;
+        for(int i=1; i<n-1; i++){
+            for(int j=0; j<m; j++){
+                sum+=matrix[i][j];
             }
         }
+        System.out.println(sum);
+        return sum;
+    }
 
-        sc.close();
+
+    public static void main(String args[]){
+
+
+        int matrix[][] = { {0,0,0},{3,3,3},{2,2,2},{1,1,1}};
+        transpose(matrix);
+        //int matrix [][]= new int[3][3];
+        //int n = matrix.length;
+        //int m = matrix[0].length;
+
+        //Scanner sc = new Scanner(System.in);
+        //for(int i=0; i<n; i++){
+        //    for(int j=0; j<m; j++){
+        //        matrix[i][j]= sc.nextInt();
+        //    }
+        //}
+
+        //sc.close();
 
         //output:- 
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                System.out.print(matrix[i][j] + " ");
-            }
-            System.out.println();
-        } 
-        search(matrix, 1);
+        //for(int i=0; i<n; i++){
+        //    for(int j=0; j<m; j++){
+        //        System.out.print(matrix[i][j] + " ");
+        //    }
+        //    System.out.println();
+        //} 
+        //search(matrix, 1);
     }
 
 }
